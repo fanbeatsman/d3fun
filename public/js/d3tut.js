634da2bd4fe4b00b88b107af4a145cad83f49a21
fanbeatsman <fan_ma8@hotmail.com>
2014-11-01 11:37:19 -0400

    tut done

--- public/js/d3tut.js
+++ public/js/d3tut.js
@@ -31,7 +31,7 @@ scaleMins = d3.scale.linear().domain([0, 59 + 59/60]).range([0, 2 * pi]);
 scaleHours = d3.scale.linear().domain([0, 11 + 59/60]).range([0, 2 * pi]);
 
 var vis, clockGroup;
-var data =[4,5,66,7,88]
+
 vis = d3.selectAll(".chart")
   .append("svg:svg")
   .attr("width", width)
@@ -52,12 +52,92 @@ clockGroup.append("svg:circle")
   .attr("class", "clock innercircle");
 
 
-d3.select(".barchart")
+var render = function(data){
+	var hourArc, minuteArc, secondArc;
+	clockGroup.selectAll(".clockhand").remove();
+	secondArc=d3.svg.arc()
+		.innerRadius(0)
+		.outerRadius(70)
+		.startAngle(function(d){
+			return scaleSecs(d.numeric);
+		})
+		.endAngle(function(d){
+			return scaleSecs(d.numeric);
+		});
+
+	minuteArc=d3.svg.arc()
+		.innerRadius(0)
+		.outerRadius(70)
+		.startAngle(function(d){
+			return scaleMins(d.numeric); //wut? no specifying array index? theres 3 numeric keys
+		})
+		.endAngle(function(d){
+			return scaleMins(d.numeric);
+		});
+
+	hourArc=d3.svg.arc()
+		.innerRadius(0)
+		.outerRadius(50)
+		.startAngle(function(d){
+			return scaleHours(d.numeric%12);
+		})
+		.endAngle(function(d) {
+    return scaleHours(d.numeric % 12);
+  });
+
+	clockGroup.selectAll(".clockhand")
+	.data(data)
+	.enter()
+	.append("svg:path")
+	.attr("d", function(d){//<g transform></> attr d is a string containing a bunch of path descriptions like MoveTo CurveTo..
+		if (d.unit === "seconds"){
+			return secondArc(d);
+		}else if (d.unit === "minutes"){
+			return minuteArc(d);
+		}else if (d.unit === "hours"){
+			return hourArc(d);
+		}
+	})
+	.attr("class", "clockhand")
+	.attr("stroke", "black")
+	.attr("stroke-width", function(d){
+		if (d.unit === "seconds"){
+			return 2;
+		}else if (d.unit === "minutes"){
+			return 3;
+		}else if (d.unit === "hours"){
+			return 4;
+		}
+	})
+	.attr("fill","none");
+
+};
+
+setInterval(function(){
+	var data;
+	data = fields();
+	return render(data);
+},1000);
+
+
+
+var data =[4,5,66,7,88]
+var x = d3.scale.linear()
+	.domain([0,d3.max(data)])
+	.range([0,d3.max(data)*10]);
+
+
+visavis = d3.select(".barchart")
 .selectAll("div")
-.data(data)
+.data(data) //is .data made to pass down "data" as the variable d?
 .enter().append("div")
-.style("width",function(d){return d*10+"px";})
+.style("width",function(d){return (x(d)+"px");})
 .text(function(d){return d;});
+
+visavis.append("svg:svg")
+	.attr("height",height)
+	.attr("width",width);
+
 $(function(){
 	console.log("QRT");
 });
